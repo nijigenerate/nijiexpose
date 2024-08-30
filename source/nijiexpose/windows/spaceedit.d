b7ec75f5d692616d804e8678a71b9e6d6c2cb9c5
Lin, Yong Xiang <r888800009@gmail.com>
2024-08-30 16:16:29 +0800

    Expose the delete button to make sure users notice

--- source/nijiexpose/windows/spaceedit.d
+++ source/nijiexpose/windows/spaceedit.d
@@ -56,16 +56,18 @@ private:
 
     string[string][Adaptor] options;
 
+    void adaptorDelete(size_t idx) {
+        // stop source on delete
+        if (editingZone.sources[idx]) editingZone.sources[idx].stop();
+        editingZone.sources = editingZone.sources.remove(idx);
+    }
+
     void adaptorMenu(size_t idx) {
         uiImRightClickPopup("AdaptorPopup");
 
         if (uiImBeginPopup("AdaptorPopup")) {
-            if (uiImMenuItem(__("Delete"))) {
-
-                // stop source on delete
-                if (editingZone.sources[idx]) editingZone.sources[idx].stop();
-                editingZone.sources = editingZone.sources.remove(idx);
-            }
+            if (uiImMenuItem(__("Delete")))
+                adaptorDelete(idx);
             uiImEndPopup();
         }
     }
@@ -280,6 +282,10 @@ public:
                                                 }
                                             }
 
+                                            // Expose the delete button to make sure users notice it.
+                                            uiImSameLine(0, 40);
+                                            if (uiImButton(__("Delete")))
+                                                adaptorDelete(i);
                                         uiImUnindent();
                                     } else {
                                         adaptorMenu(i);
